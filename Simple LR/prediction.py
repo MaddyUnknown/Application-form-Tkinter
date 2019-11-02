fa60db5b56f477feb5a1998537e68d827ec1b954
Rishab Mallick <mallickrishab@gmail.com>
2019-11-02 21:16:31 +0530

    Add files via upload

---
+++ Simple LR/prediction.py
@@ -0,0 +1,158 @@
+import pandas as pd
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('random.csv')
+
+Y=data.result
+X=data.drop('result',axis=1)
+
+X,Y = X.to_numpy(), Y.to_numpy()
+Y = Y.reshape((-1,1))
+X,Y = X.T, Y.T
+
+X_train, X_test = X[:,:800], X[:,801:]
+Y_train, Y_test = Y[:,:800], Y[:,801:]
+
+def sigmoid(x):
+    return  1 / (1+ np.exp(-x))
+
+def initialize_with_zeros(dim):
+    w = np.zeros((dim, 1))
+    b = 0
+
+    assert(w.shape == (dim, 1))
+    assert(isinstance(b, float) or isinstance(b, int))
+    
+    return w, b
+
+def propagate(w, b, X, Y):
+    epsilon = 1e-5  
+    m = X.shape[1]
+    
+    # FORWARD PROPAGATION (FROM X TO COST)
+    A = sigmoid((w.T @ X) + b)                                    # compute activation
+    cost = (- 1/m) * np.sum(Y*np.log(A+epsilon) + (1-Y)*(np.log(1-A+epsilon)))                             # compute cost
+
+    
+    # BACKWARD PROPAGATION (TO FIND GRAD)
+    dw = (1/m) * (X @ (A-Y).T)
+    db = (1/m) * np.sum(A - Y)
+
+    assert(dw.shape == w.shape)
+    assert(db.dtype == float)
+    cost = np.squeeze(cost)
+    assert(cost.shape == ())
+    
+    grads = {"dw": dw,
+             "db": db}
+    
+    return grads, cost
+
+def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost = False):
+    """
+    This function optimizes w and b by running a gradient descent algorithm
+ 
+    """
+    
+    costs = []
+    
+    for i in range(num_iterations):
+        
+        
+        # Cost and gradient calculation (≈ 1-4 lines of code)
+        grads, cost = propagate(w, b, X, Y)
+        
+        # Retrieve derivatives from grads
+        dw = grads["dw"]
+        db = grads["db"]
+        
+        # update rule (≈ 2 lines of code)
+        w = w - learning_rate*dw
+        b = b - learning_rate*db
+        
+        # Record the costs
+        if i % 100 == 0:
+            costs.append(cost)
+        
+        # Print the cost every 100 training iterations
+        if print_cost and i % 100 == 0:
+            print ("Cost after iteration %i: %f" %(i, cost))
+    
+    params = {"w": w,
+              "b": b}
+    
+    grads = {"dw": dw,
+             "db": db}
+    
+    return params, grads, costs
+
+def predict(w, b, X):
+    '''
+    Predict whether the label is 0 or 1 using learned logistic regression parameters (w, b)
+    '''
+    
+    m = X.shape[1]
+    Y_prediction = np.zeros((1,m))
+    w = w.reshape(X.shape[0], 1)
+    
+    # Compute vector "A" predicting the probabilities of a cat being present in the picture
+    A = sigmoid((w.T @ X) + b)
+    
+    for i in range(A.shape[1]):
+        
+        # Convert probabilities A[0,i] to actual predictions p[0,i]
+        Y_prediction = np.trunc(A + np.copysign(0.5, A)) #doesn't needs to be in a for loop
+    
+    assert(Y_prediction.shape == (1, m))
+    
+    return Y_prediction
+
+def model(X_train, Y_train, X_test, Y_test, num_iterations = 2000, learning_rate = 0.5, print_cost = False):
+    """
+    Builds the logistic regression model by calling the function you've implemented previously
+    """
+    
+    # initialize parameters with zeros (≈ 1 line of code)
+    w, b = initialize_with_zeros(X_train.shape[0])
+
+    # Gradient descent (≈ 1 line of code)
+    parameters, grads, costs = optimize(w, b, X_train, Y_train, num_iterations, learning_rate, print_cost)
+    
+    # Retrieve parameters w and b from dictionary "parameters"
+    w = parameters["w"]
+    b = parameters["b"]
+    
+    # Predict test/train set examples (≈ 2 lines of code)
+    Y_prediction_test = predict(w, b, X_test)
+    Y_prediction_train = predict(w, b, X_train)
+
+
+    # Print train/test Errors
+    # print("train accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_train - Y_train)) * 100))
+    # print("test accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_test - Y_test)) * 100))
+
+    
+    d = {"costs": costs,
+         "Y_prediction_test": Y_prediction_test, 
+         "Y_prediction_train" : Y_prediction_train, 
+         "w" : w, 
+         "b" : b,
+         "learning_rate" : learning_rate,
+         "num_iterations": num_iterations}
+    
+    return d
+
+# Training the model
+d = model(X_train, Y_train, X_test, Y_test, num_iterations = 30000,\
+     learning_rate = 0.01, print_cost = False)
+
+def probability(phy, chem, math):
+    '''
+    Prints the probability of someone getting accepted 
+    into the college.
+    '''
+    A = np.array([phy,chem,math])
+    prob = sigmoid((d['w'].T @ A) + d['b'])
+    print('Your probability of getting accepted into the college is:  ' + str(prob * 100))
